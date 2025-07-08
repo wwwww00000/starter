539380f61f90072888d859b0fade8ebe35b3c6ee
wenbin <limwenbin94@hotmail.com>
2025-07-08 20:19:10 +0800

    minimal config for p12n

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
         ---@type lspconfig.options
         servers = {
           -- pyright will be automatically installed with mason and loaded with lspconfig
-          pyright = {},
+          -- pyright = {},
         },
       },
     },
