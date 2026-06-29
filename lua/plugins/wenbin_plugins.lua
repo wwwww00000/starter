16efc46a65cb97b7978ad1e1320f71cb6297711d
wenbin <limwenbin94@hotmail.com>
2026-06-29 21:12:16 +0800

    Add diffview.nvim

--- lua/plugins/wenbin_plugins.lua
+++ lua/plugins/wenbin_plugins.lua
@@ -1,5 +1,23 @@
 return {
-  -- change some telescope options and a keymap to browse plugin files
   "tpope/vim-fugitive",
   "tpope/vim-sleuth",
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewLog",
+      "DiffviewOpen",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Working Tree" },
+      { "<leader>gD", "<cmd>DiffviewOpen HEAD^!<cr>", desc = "Diffview Last Commit" },
+      { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview Repo History" },
+    },
+  },
 }
